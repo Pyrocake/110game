48f74a20808c160efcebb950eceb32ff99387911
booktail <tailbook7@gmail.com>
2023-01-19 22:43:56 -0500

    Player code trimmed.

--- Player/Player.cs
+++ Player/Player.cs
@@ -82,18 +82,6 @@ public class Player : KinematicBody2D
 		//}
 
 		animationState.Travel("Attack");
-		//var inputVector = new Vector2();
-		//inputVector.x = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
-		//inputVector.y = Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up");
-		//inputVector = inputVector.Normalized();
-		//if (inputVector != Vector2.Zero) {
-		//	velocity = velocity.MoveToward(inputVector * MAX_SPEED * .25f, ACCELERATION * delta);
-//
-		//	animationTree.Set("parameters/Idle/blend_position", inputVector);
-		//	animationTree.Set("parameters/Run/blend_position", inputVector);
-		//} else {
-		//	velocity = velocity.MoveToward(Vector2.Zero, FRICTION * delta);
-		//}
 		
 		velocity = velocity.MoveToward(Vector2.Zero, FRICTION * delta);
 		velocity = MoveAndSlide(velocity);
